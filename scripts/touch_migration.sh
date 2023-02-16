13c18b5544cc5b4aac61506a3ebb73a8b20c0cec
Jere Penttinen <jere.penttinen@tuni.fi>
2023-02-16 11:21:55 +0200

    Remove spaces from arg and ensure dir exists

--- scripts/touch_migration.sh
+++ scripts/touch_migration.sh
@@ -5,9 +5,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-file_name="$(echo "$1" | tr '[:upper:]' '[:lower:]')"
+file_name="$(echo "$1" | tr '[:upper:]' '[:lower:]' | sed -E 's/[[:space:]]+/_/g')"
 
 scripts_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd "$scripts_path" || exit 2
+
+# Ensure dir exists
+mkdir -p "../src/main/resources/db/migration"
 
 touch "../src/main/resources/db/migration/V$(date +"%Y%m%d%H%M%S")__${file_name}.sql"
