fb22edc56d8ef302c3c29ae53b4ceb5307ae5dbf
Ilari Ijas <ilari.ijas@tuni.fi>
2023-02-22 17:22:12 +0200

    Etusivun ruokakomponentti

--- frontend/src/components/FrontPageRecipe.tsx
+++ frontend/src/components/FrontPageRecipe.tsx
@@ -2,6 +2,7 @@ import Col from 'react-bootstrap/Col';
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Figure from "react-bootstrap/Figure";
+import { Link } from "react-router-dom";
 
 export interface Props{
     header?: string;
@@ -11,14 +12,17 @@ const FrontPageRecipe = ({ header }: Props) => {
 
     return(
         <Col xs={4} style={{margin: 10, marginLeft: 0}}>
-            <Figure>
-                <Figure.Image src='https://4.bp.blogspot.com/-ecGN1GaoT-g/Uz20paShlSI/AAAAAAAAAcc/mc7Or69LsNw/s1600/veghamp2.jpg'
+            <Link to='/'>
+                <Figure>
+                    <Figure.Image src='https://4.bp.blogspot.com/-ecGN1GaoT-g/Uz20paShlSI/AAAAAAAAAcc/mc7Or69LsNw/s1600/veghamp2.jpg'
+                        rounded
+                    />
+                    <p className="fs-3 fw-bold text-center text-break text-dark">
+                        Hampurilaisateria
+                    </p>
+                </Figure>
+            </Link>
 
-                />
-                <p className="fs-3 fw-bold text-center text-break">
-                    Hampurilaisateria
-                </p>
-            </Figure>
 
 
         </Col>
