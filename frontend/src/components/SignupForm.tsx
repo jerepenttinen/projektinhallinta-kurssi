b33c8c870642dde9d29a286c332921993eed0dec
Jere Penttinen <jere.penttinen@tuni.fi>
2023-02-24 08:50:08 +0200

    Vite

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,23 +1,20 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useRef } from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useRef } from "react";
 import LoginUser from "../core/domain/loginUser";
 
-const env = process.env
+const env = import.meta.env;
 
 const SignupForm = () => {
-
   const formInputElements = {
     usernameRef: useRef<HTMLInputElement>(null),
     firstNameRef: useRef<HTMLInputElement>(null),
     lastNameRef: useRef<HTMLInputElement>(null),
     emailRef: useRef<HTMLInputElement>(null),
     passwordRef: useRef<HTMLInputElement>(null),
-  }
-
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-
     event.preventDefault();
 
     /*TODO
@@ -26,35 +23,55 @@ const SignupForm = () => {
 
     const user = new LoginUser(formInputElements);
 
-    const url = env.REACT_APP_BACKEND_URL + "/api/users/signup";
+    const url = env.VITE_APP_BACKEND_URL + "/api/users/signup";
 
-    try{
-        // const res = await axios.post(url, user);
-        // const data = await res.json();
-        // Tai fetch, jos ei käytetä axiosta
-    } catch(err) {
-        // handle error
-        console.log(err);
+    try {
+      // const res = await axios.post(url, user);
+      // const data = await res.json();
+      // Tai fetch, jos ei käytetä axiosta
+    } catch (err) {
+      // handle error
+      console.log(err);
     }
   };
   return (
     <Form className="d-grid gap-4" onSubmit={handleSubmit}>
       <h2>Rekisteröidy</h2>
       <Form.Group className="mb-3" controlId="username">
-        <Form.Control type="text" placeholder="Käyttäjänimi" ref={formInputElements.usernameRef}/>
+        <Form.Control
+          type="text"
+          placeholder="Käyttäjänimi"
+          ref={formInputElements.usernameRef}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="fname">
-        <Form.Control type="text" placeholder="Etunimi" ref={formInputElements.firstNameRef}/>
+        <Form.Control
+          type="text"
+          placeholder="Etunimi"
+          ref={formInputElements.firstNameRef}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="lname">
-        <Form.Control type="text" placeholder="Sukunimi" ref={formInputElements.lastNameRef}/>
+        <Form.Control
+          type="text"
+          placeholder="Sukunimi"
+          ref={formInputElements.lastNameRef}
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="email">
-        <Form.Control type="email" placeholder="Sähköposti" ref={formInputElements.emailRef}/>
+        <Form.Control
+          type="email"
+          placeholder="Sähköposti"
+          ref={formInputElements.emailRef}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="password">
-        <Form.Control type="password" placeholder="Salasana" ref={formInputElements.passwordRef}/>
+        <Form.Control
+          type="password"
+          placeholder="Salasana"
+          ref={formInputElements.passwordRef}
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit" size="lg">
@@ -62,6 +79,6 @@ const SignupForm = () => {
       </Button>
     </Form>
   );
-}
+};
 
 export default SignupForm;
