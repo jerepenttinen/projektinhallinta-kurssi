56fb1763fddef43667b2435993c6583a818a3087
Eero <eero.kokkonen@hotmail.com>
2023-03-07 10:31:13 +0200

    Added possibility to see image when dropping it

--- frontend/src/pages/ProfileSettingsPage.tsx
+++ frontend/src/pages/ProfileSettingsPage.tsx
@@ -1,33 +1,36 @@
-import FileDropzone from '../components/FileDropzone'
+import DropImages from '../components/DropImages'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 
 
 const ProfileSettingsPage = () => {
     const [image, setImage] = useState<File[]>([]);
+    const [imageUrl, setImageUrl] = useState<String>();
+    const onImageDropped = (buffer: Uint8Array) =>{
 
-    useEffect(() => {
-        console.log(image[0])
-
-    }, [image]);
+        console.log(buffer);
+        const blob = new Blob([buffer]);
+        setImageUrl(URL.createObjectURL( blob ));
+        console.log(blob);
+    }
 
     return (
       <div className="m-auto bg-white p-4" style={{width:900}}>
           <h2>Asetukset</h2>
           <Form className="">
-              <div className="my-4 d-flex justify-content-between" style={{height:150}}>
+              <div className="my-4 d-flex justify-content-between " style={{height:150}}>
                   <div style={{width:'15%'}}>
-                      <img src="https://secure.gravatar.com/avatar/5586197d3539ebe07272af21926b496f?s=1920&d=mm&r=g"
-                         className="rounded-circle h-100"
-                         style={{width:'100%', maxHeight:'100%'}} >
+                      <img src={imageUrl ? imageUrl : "https://secure.gravatar.com/avatar/5586197d3539ebe07272af21926b496f?s=1920&d=mm&r=g"}
+                         className="rounded-circle"
+                         style={{height:'100%'}} >
                        </img>
                   </div>
                   <div className="w-75 h-100" style={{minHeight:'100%'}}>
-                      <FileDropzone setImage={setImage}/>
+                      <DropImages onImageDropped={onImageDropped}/>
                   </div>
               </div>
               <Form.Group className="mb-3" controlId="textArea">
