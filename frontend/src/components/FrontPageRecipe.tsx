8e5c4e74fe74c3b754dd7dc49e2ae6b90b913315
Eero <eero.kokkonen@hotmail.com>
2023-03-14 16:37:21 +0200

    Added possbility to change size of recipe column

--- frontend/src/components/FrontPageRecipe.tsx
+++ frontend/src/components/FrontPageRecipe.tsx
@@ -5,20 +5,20 @@ import Figure from "react-bootstrap/Figure";
 import { Link } from "react-router-dom";
 
 export interface Props{
-    header?: string;
+    header?: string,
+    xs?: number,
 
 }
-const FrontPageRecipe = ({ header }: Props) => {
-
+const FrontPageRecipe = ({ header, xs }: Props) => {
     return(
-        <Col xs={4} style={{margin: 10, marginLeft: 0}}>
+        <Col xs={xs} className="" style={{margin: 10, marginLeft: 0}}>
             <Link to='/'>
                 <Figure>
                     <Figure.Image src='https://4.bp.blogspot.com/-ecGN1GaoT-g/Uz20paShlSI/AAAAAAAAAcc/mc7Or69LsNw/s1600/veghamp2.jpg'
                         rounded
                     />
-                    <p className="fs-3 fw-bold text-center text-break text-dark">
-                        Hampurilaisateria
+                    <p className="fs-4 fw-bold text-center text-break text-dark" >
+                        {header}
                     </p>
                 </Figure>
             </Link>
