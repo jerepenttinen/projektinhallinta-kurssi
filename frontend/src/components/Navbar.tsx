e4dd70c98417f0443f9cbe5cb4c7a8a6d18868ad
Jere Penttinen <jere.penttinen@tuni.fi>
2023-03-15 10:47:21 +0200

    Add brand link to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { Navbar, Nav } from 'react-bootstrap';
-import {FaBook} from 'react-icons/fa';
+import { Navbar, Nav } from "react-bootstrap";
+import { FaBook } from "react-icons/fa";
 import "./css/Navbar.css";
 import { Link } from "react-router-dom";
 
 type MyNavbarProps = {
-    right?: () => React.ReactElement
+  right?: () => React.ReactElement;
 };
 
-function MyNavbar({right}: MyNavbarProps) {
-    return (
-        <Navbar bg="light" className="px-3 shadow" expand="lg" sticky="top" >
-                <Navbar.Brand href="#home" className="brand">
-                    <FaBook/> Keittokirja
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link to="/"  className="nav-link" >Etusivu</Link>
-                        <Link to="/search"  className="nav-link" >Haku</Link>
-                    </Nav>
-                    <Nav>
-                        {right && right()}
-                    </Nav>
-                </Navbar.Collapse>
-        </Navbar>
-    );
+function MyNavbar({ right }: MyNavbarProps) {
+  return (
+    <Navbar bg="light" className="px-3 shadow" expand="lg" sticky="top">
+      <Link to="/" className="navbar-brand brand">
+        <FaBook /> Keittokirja
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Link to="/" className="nav-link">
+            Etusivu
+          </Link>
+          <Link to="/search" className="nav-link">
+            Haku
+          </Link>
+        </Nav>
+        <Nav>{right && right()}</Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default MyNavbar;
