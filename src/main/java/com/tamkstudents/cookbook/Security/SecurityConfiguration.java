cb423fbd15ae4ab886b189393e77156ed5a4598e
Jere Penttinen <jere.penttinen@tuni.fi>
2023-03-15 11:36:01 +0200

    Make all cookies SameSite=Strict

--- src/main/java/com/tamkstudents/cookbook/Security/SecurityConfiguration.java
+++ src/main/java/com/tamkstudents/cookbook/Security/SecurityConfiguration.java
@@ -1,7 +1,12 @@
 package com.tamkstudents.cookbook.Security;
 
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.http.CookieProcessor;
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -46,4 +51,13 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    // Make all cookies SameSite=Strict
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (factory) -> factory.addContextCustomizers((context) -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.STRICT.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        });
+    }
 }
