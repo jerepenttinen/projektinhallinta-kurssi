cb49ca6145eb7a99b6a398546088425f7b6c3ee6
Jere Penttinen <jere.penttinen@tuni.fi>
2023-03-31 17:27:45 +0300

    Add test

--- src/main/java/com/tamkstudents/cookbook/Controller/Request/CreateReviewRequest.java
+++ src/main/java/com/tamkstudents/cookbook/Controller/Request/CreateReviewRequest.java
@@ -1,13 +1,16 @@
 package com.tamkstudents.cookbook.Controller.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Value;
 
+@Builder
 @Value
 public class CreateReviewRequest {
     @NotBlank
     String content;
 
-    @NotBlank
+    @NotNull
     Boolean isUpvote;
 }
