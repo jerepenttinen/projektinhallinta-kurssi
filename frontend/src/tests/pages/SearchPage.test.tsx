64d8f334a044860e11072313d1e20cc4d6d6f4d5
Eero <eero.kokkonen@hotmail.com>
2023-04-05 18:16:06 +0300

    Lisää testejä sivuille

---
+++ frontend/src/tests/pages/SearchPage.test.tsx
@@ -0,0 +1,14 @@
+import SearchPage from "../../pages/SearchPage"
+import { describe, test, expect } from "vitest"
+import { render, screen } from "@testing-library/react";
+
+describe('Searchpage', () => {
+    test("should show searchpage", () => {
+        const wrapper = render(<SearchPage/>);
+        expect(wrapper).toBeTruthy();
+
+        const h1 = wrapper.container.querySelector("h1");
+        expect(h1?.textContent).toBe("Haku");
+        screen.debug();
+    });
+});
---
+++ frontend/src/tests/pages/SignInPage.test.tsx
@@ -0,0 +1,14 @@
+import SignInPage from "../../pages/SignInPage"
+import { describe, test, expect } from "vitest"
+import { render, screen } from "@testing-library/react";
+
+describe('Signinpage', () => {
+    test("should show signinpage", () => {
+        const wrapper = render(<SignInPage/>);
+        expect(wrapper).toBeTruthy();
+
+        const h2 = wrapper.container.querySelector("h2");
+        expect(h2?.textContent).toBe("Kirjaudu");
+        screen.debug();
+    });
+});
